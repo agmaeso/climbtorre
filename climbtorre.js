76a937669daba2aa173852cb338126a56447dc0b
Antonio Garcia Maeso <agmaeso@Antonios-MacBook-Air.local>
2016-02-02 20:10:49 +0100

    Desplazado 'fortune cookie' a nuevo modulo.

--- climbtorre.js
+++ climbtorre.js
@@ -1,50 +1,42 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
- .create({ defaultLayout:'main' });
+	.create({ defaultLayout:'main' });
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
-var fortunes = [
- "Conquer your fears or they will conquer you.",
- "Rivers need springs.",
- "Do not fear what you don't know.",
- "You will have a pleasant surprise.",
- "Whenever possible, keep it simple.",
-];
 
 app.set('port', process.env.PORT || 4000);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
- res.render('home');
- // res.status(200) es implícito
+	res.render('home');
+	// res.status(200) es implícito
 });
 
 app.get('/about', function(req, res){
-var randomFortune =
- fortunes[Math.floor(Math.random() * fortunes.length)];
- res.render('about', { fortune: randomFortune });
+	res.render('about', { fortune: fortune.getFortune() });
 });
 
 // error route: custom 404 page
 app.use(function(req, res){
- res.status(404);
- res.render('404');
+	res.status(404);
+	res.render('404');
 });
 
 // error route: custom 500 page
 app.use(function(err, req, res, next){
- console.error(err.stack);
- res.status(500);
- res.render('500');
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
- console.log( 'Express started on http://localhost:' +
- app.get('port') + '; press Ctrl-C to terminate.' );
+	console.log( 'Express started on http://localhost:' +
+	app.get('port') + '; press Ctrl-C to terminate.' );
 });
---
+++ readme.md
@@ -0,0 +1,9 @@
+#Readme.md
+Ni puta idea de como crear un fichero markdown
+
+#Ejemplo de texto
+(function(){
+    var pepe: string;
+    return true
+})()
+
